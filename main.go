d41136b4e17603382c8d14d7f784b96ed25ba22f
craftman2868 <57369130+Craftman2868@users.noreply.github.com>
2025-06-15 19:40:55 +0200

    Utilisation du package go-libs/app dans main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 
-	"github.com/Craftman2868/go-libs/clock"
+	"github.com/Craftman2868/go-libs/app"
 	"github.com/Craftman2868/go-libs/event"
 	"github.com/Craftman2868/go-libs/terminal"
 )
@@ -18,29 +18,36 @@ const (
 )
 
 var showFps = false
-var parser terminal.Parser
-var handler event.BaseHandler
-var appClock clock.Clock
 
-func main() {
-	terminal.Init()
-
-	terminal.SetStyle("0")
-	// terminal.SetStyle("0;1")
-	appClock = clock.NewClock(MAX_FPS)
-
-	handler.On("key", on_key)
-
-	parser = terminal.NewParser(&handler)
-
-	run()
-
-	// terminal.SetStyle("0")
-
-	terminal.Quit()
+type RainApp struct {
+	app.TerminalApp
+	showFps      bool
+	drops        []drop
+	screenBuf    []byte
+	termW, termH uint16
 }
 
-func on_key(ev_ event.Event) {
+type drop struct {
+	x, y  uint16
+	char  byte
+	speed uint16
+}
+
+func NewRainApp() *RainApp {
+	var ap RainApp
+
+	ap.TerminalApp = app.NewTerminalApp()
+
+	ap.On("key", ap.on_key)
+
+	ap.On("update", func(event.Event) {
+		ap.update()
+	})
+
+	return &ap
+}
+
+func (app *RainApp) on_key(ev_ event.Event) {
 	ev := ev_.(terminal.KeyEvent)
 
 	switch ev.Key {
@@ -52,144 +59,102 @@ func on_key(ev_ event.Event) {
 	case terminal.KEY_ESC:
 		fallthrough
 	case 'Q':
-		running = false
+		app.Stop()
 	case 'F':
-		showFps = !showFps
+		app.showFps = !app.showFps
 	}
 }
 
-type drop struct {
-	x, y  uint16
-	char  byte
-	speed uint16
-}
-
-var running bool
-var drops []drop
-
-func run() {
-	running = true
-
-	for running {
-		update()
-		appClock.TickSleep()
-	}
-}
-
-var termW, termH uint16
-
-func randDropChar() byte {
+func (app *RainApp) randDropChar() byte {
 	n := rand.Int31n(DROP_CHAR_COUNT - 1)
-
 	return DROP_CHARS[n]
 }
 
-func newDrop() {
+func (app *RainApp) newDrop() {
 	var d drop
-
-	d.x = uint16(rand.Int31n(int32(termW - 1)))
+	d.x = uint16(rand.Int31n(int32(app.termW - 1)))
 	d.y = uint16(rand.Int31n(10))
-	d.char = randDropChar()
+	d.char = app.randDropChar()
 	d.speed = uint16(rand.Int31n(3) + 1)
-
-	drops = append(drops, d)
+	app.drops = append(app.drops, d)
 }
 
-var screenBuf []byte
-
-func initScreenBuf() {
-	screenBuf = make([]byte, int(termW)*int(termH))
+func (app *RainApp) initScreenBuf() {
+	app.screenBuf = make([]byte, int(app.termW)*int(app.termH))
 }
 
-func clearScreenBuf() {
-	for i := range screenBuf {
-		screenBuf[i] = ' '
+func (app *RainApp) clearScreenBuf() {
+	for i := range app.screenBuf {
+		app.screenBuf[i] = ' '
 	}
 }
 
-func checkDropsPos() {
-	for i := 0; i < len(drops); i++ {
-		if drops[i].x >= termW || drops[i].y >= termH {
-			drops[i] = drops[len(drops)-1]
-			drops = drops[:len(drops)-1]
+func (app *RainApp) checkDropsPos() {
+	for i := 0; i < len(app.drops); i++ {
+		if app.drops[i].x >= app.termW || app.drops[i].y >= app.termH {
+			app.drops[i] = app.drops[len(app.drops)-1]
+			app.drops = app.drops[:len(app.drops)-1]
 			i--
 		}
 	}
 }
 
-func writeScreenBuf(x, y int, data []byte) {
-	// screenBuf[y*int(termW)+x: math.Min(y*int(termW)+x+len(data), len(screenBuf))] = data[:math.Min(len(data), len(screenBuf)-(y*int(termW)+x))]
-	p := y*int(termW) + x
-
+func (app *RainApp) writeScreenBuf(x, y int, data []byte) {
+	p := y*int(app.termW) + x
 	for i := range data {
-		if p+i >= len(screenBuf) {
+		if p+i >= len(app.screenBuf) {
 			break
 		}
-		screenBuf[p+i] = data[i]
+		app.screenBuf[p+i] = data[i]
 	}
 }
 
-func update() {
-	// Update initialization
+func (app *RainApp) update() {
+	app.termW, app.termH = terminal.GetSize()
+	app.termH--
 
-	termW, termH = terminal.GetSize()
-	termH--
-
-	if termH < 10 {
-		running = false
+	if app.termH < 10 {
+		app.Stop()
 		return
 	}
 
-	if len(screenBuf) != int(termW)*int(termH) {
-		initScreenBuf()
-		checkDropsPos()
+	if len(app.screenBuf) != int(app.termW)*int(app.termH) {
+		app.initScreenBuf()
+		app.checkDropsPos()
 	}
 
-	clearScreenBuf()
+	app.clearScreenBuf()
 
-	// Handle inputs
-
-	parser.HandleInputs()
-
-	// Process rain
-
-	for i := 0; i < MAX_NEW_DROP_COUNT && len(drops) < MAX_DROP_COUNT; i++ {
-		newDrop()
+	for i := 0; i < MAX_NEW_DROP_COUNT && len(app.drops) < MAX_DROP_COUNT; i++ {
+		app.newDrop()
 	}
 
-	for i := 0; i < len(drops); i++ {
-		d := &drops[i]
-		screenBuf[int(d.y)*int(termW)+int(d.x)] = d.char
-
+	for i := 0; i < len(app.drops); i++ {
+		d := &app.drops[i]
+		app.screenBuf[int(d.y)*int(app.termW)+int(d.x)] = d.char
 		d.y += d.speed
 
-		if d.y >= termH {
-			drops[i] = drops[len(drops)-1]
-			drops = drops[:len(drops)-1]
+		if d.y >= app.termH {
+			app.drops[i] = app.drops[len(app.drops)-1]
+			app.drops = app.drops[:len(app.drops)-1]
 			i--
 		}
 	}
 
-	// Show FPS
-
-	if showFps {
-		writeScreenBuf(0, 0, []byte(fmt.Sprint(appClock.GetTps())))
+	if app.showFps {
+		app.writeScreenBuf(0, 0, []byte(fmt.Sprint(app.GetTps())))
 	}
 
-	// Render screenBuf
-
 	terminal.SetCursorHome()
-
-	for i := range int(termH) {
-		terminal.WritelnBytes(screenBuf[i*int(termW) : (i+1)*int(termW)])
+	for i := range int(app.termH) {
+		terminal.WritelnBytes(app.screenBuf[i*int(app.termW) : (i+1)*int(app.termW)])
 	}
 }
 
-/*
-main.update()
-	/home/mateo/go/projects/rain/main.go:154 +0x3a9
-main.run()
-	/home/mateo/go/projects/rain/main.go:70 +0x1c
-main.main()
-	/home/mateo/go/projects/rain/main.go:34 +0x2a8
-*/
+func main() {
+	app := NewRainApp()
+
+	app.Init(MAX_FPS)
+	app.Run()
+	app.Quit()
+}
